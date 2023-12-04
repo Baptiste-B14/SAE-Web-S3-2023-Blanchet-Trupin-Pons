a1168e1833f043c0ff0a6edf21f33efb565c1505
BaptisteB14 <blanchet.baptiste77@gmail.com>
2023-12-04 22:20:52 +0100

    commit originel

---
+++ index.php
@@ -0,0 +1,26 @@
+
+
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+
+    <title>Page de connexion</title>
+   
+    <link rel="stylesheet" href="style.css" />
+
+   
+
+  </head>
+
+  <body>
+
+   
+
+    <h1>Site web bancal </h1>
+
+    <footer>
+      <p>©Copyright Baptiste Blanchet. Tous droits reversés.</p>
+    </footer>
+  </body>
+</html>
