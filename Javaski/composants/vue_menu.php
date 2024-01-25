3fc7cc172b5167bd35123a9f20b01dc2fc149349
Baptiste-B14 <blanchet.baptiste77@gmail.com>
2024-01-25 13:16:07 +0100

    intégration page principale

--- Javaski/composants/vue_menu.php
+++ Javaski/composants/vue_menu.php
@@ -5,25 +5,20 @@ class VueMenus {
   private $affichageMenuAdministrateur;
 	public function __construct() {
 		$this->affichageMenuUtilisateur = '
-          <div class="w-col w-col-2">
-            <a href="index.php" class="navbar-brand w-nav-brand"><img src="images/Logo.png" loading="lazy" width="115" sizes="(max-width: 767px) 100vw, (max-width: 991px) 98px, 115px" alt="" srcset="images/Logo-p-500.png 500w, images/Logo-p-800.png 800w, images/Logo.png 907w"></a>
-          </div>
-          <div class="w-col w-col-2">
-            <a href="index.php?module=mod_leJeu" class="button-2 w-button">Le Jeu</a>
-          </div>
-          <div class="column-3 w-col w-col-2">
-            <a href="index.php?module=mod_leJeu" class="button-2 w-button">Le Jeu</a>
-          </div>
-          <div class="w-col w-col-2">
-            <a href="index.php?module=mod_lesTours" class="button-2 w-button">Les Tours</a>
-          </div>
-          <div class="w-col w-col-2">
-            <a href="index.php?module=mod_profil" class="button-2 w-button">Mon Profil</a>
-          </div>
-          <div class="w-col w-col-2">
-            <a href="index.php?module=mod_connexion&action=formConnexion" class="button-2 w-button">Connexion</a>
-          </div>
-        ';
+    <div class="section nav">
+    <div class="container nav">
+    <a href="#Home" class="logo-linkblock w-inline-block"><img src="images/logo_1.png" loading="lazy" width="72" alt="" class="logo"></a>
+    <div class="menu-link">
+      <a href="#index.php?module=mod_jeu" class="nav-links">LE JEU</a>
+      <a href="#index.php?module=mod_statistiques" class="nav-links">LES TOURS</a>
+      <a href="#index.php?module=mod_scores" class="nav-links">SCORES MONDIAUX</a>
+      <a href="#index.php?module=mod_profil" class="nav-links">MON PROFIL</a>
+      <a href="#index.php?module=mod_connexion" class="nav-links hover cta">CONNEXION</a>
+      </div>
+      <div data-w-id="459bebdb-edfb-018a-84a6-5afc80cd01ae" data-is-ix2-target="1" class="hamburger" data-animation-type="lottie" data-src="C:\wamp64\www\Site-Javaski\Javaski\documents\lottieflow-menu-nav-04-ffffff-easey.json" data-loop="0" data-direction="1" data-autoplay="0" data-renderer="svg" data-default-duration="2.5833333333333335" data-duration="0"></div>
+        
+      </div>
+      </div>';
 
     $this->affichageMenuAdministrateur = '
           <div class="w-col w-col-2">
--- Javaski/template.php
+++ Javaski/template.php
@@ -14,46 +14,43 @@
   <link href="images/favicon.ico" rel="shortcut icon" type="image/x-icon">
   <link href="images/webclip.png" rel="apple-touch-icon">
 </head>
-<body>
-	<header> 
-	</header>
-	<body class = "body">
-		
-		<div data-animation="default" data-collapse="all" data-duration="400" data-easing="ease" data-easing2="ease" role="banner" class="navbar-logo-left-container shadow-three w-nav">
-		<div class="w-layout-blockcontainer w-container">
-			<div class="columns w-row">
-		<?php
+	<body class = "bodycommun">
+	
 			
-			if (empty($_SESSION["login"])){
-				echo $compMenu->affiche();
-				// CHANGER LES ECHO SUIVANTS et les mettre en div class
-				echo '<div class="w-col w-col-2"><a href="index.php?module=mod_connexion&action=formConnexion class="navbar-brand w-nav-brand"">formulaire connexion </a></div>';
-			}else if ($compMenu->userAdministrateur()){
-				echo $compMenu->affiche();
-				echo '<div class="w-col w-col-2"><a href="index.php?module=mod_outilsAdministrateurs class="navbar-brand w-nav-brand"">deconnexion </a></div>';
-				echo '<div class="w-col w-col-2"><a href="index.php?module=mod_connexion&action=deconnexion class="navbar-brand w-nav-brand"">deconnexion </a></div>';
-			}else{
-				echo $compMenu->affiche();
-				echo '<div class="w-col w-col-2"><a href="index.php?module=mod_connexion&action=deconnexion class="navbar-brand w-nav-brand"">deconnexion </a></div>';
-			}
+     
+				<?php
+					
+					if (empty($_SESSION["login"])){
+						echo $compMenu->affiche();
+						// CHANGER LES ECHO SUIVANTS et les mettre en div class
+						echo '<div class="w-col w-col-2"><a href="index.php?module=mod_connexion&action=formConnexion class="navbar-brand w-nav-brand"">formulaire connexion </a></div>';
+					}else if ($compMenu->userAdministrateur()){
+						echo $compMenu->affiche();
+						echo '<div class="w-col w-col-2"><a href="index.php?module=mod_outilsAdministrateurs class="navbar-brand w-nav-brand"">deconnexion </a></div>';
+						echo '<div class="w-col w-col-2"><a href="index.php?module=mod_connexion&action=deconnexion class="navbar-brand w-nav-brand"">deconnexion </a></div>';
+					}else{
+						echo $compMenu->affiche();
+						echo '<div class="w-col w-col-2"><a href="index.php?module=mod_connexion&action=deconnexion class="navbar-brand w-nav-brand"">deconnexion </a></div>';
+					}
+					?>
+				
+				
+
+				<?php
+				
+					echo $affichageModule;
+				?>
 			
-		?>
-			</div>
-		</div>
-		</div>
-			<?php
-				echo $affichageModule;
-			?>
-	</body>
+	
+	
 	<footer>
 		<?php
-			echo'<section class="footer-subscribe">
+			echo ' <section class="footer-subscribe">
 			<div class="container">
 			  <div class="footer-wrapper-three">
 				<div class="footer-block-three">
 				  <a href="#" class="footer-link-three">A propos</a>
 				  <a href="#" class="footer-link-three">Features</a>
-				  <a href="#" class="footer-link-three"></a>
 				</div>
 				<div class="footer-social-block-three">
 				  <a href="#" class="footer-social-link-three w-inline-block"><img src="https://uploads-ssl.webflow.com/62434fa732124a0fb112aab4/62434fa732124a705912aaeb_facebook%20big%20filled.svg" loading="lazy" alt=""></a>
@@ -63,16 +60,18 @@
 				</div>
 			  </div>
 			  <div class="footer-divider-two"></div>
-			  <div class="footer-bottom">
-				<div class="footer-copyright">© 2023 Javaski.inc. Tous droits réservés - Auteurs : Baptiste BLANCHET, Matthias TRUPIN, Gaspard PONS</div>
-				<div class="footer-legal-block">
-				  <a href="#" class="footer-legal-link">Terms Of Use</a>
-				  <a href="#" class="footer-legal-link">Privacy Policy</a>
+				<div class="footer-bottom">
+				  <div class="footer-copyright">© 2023 Javaski.inc. Tous droits réservés</div>
+				  <div class="footer-legal-block">
+					<a href="#" class="footer-legal-link">Terms Of Use</a>
+					<a href="#" class="footer-legal-link">Privacy Policy</a>
+				  </div>
 				</div>
 			  </div>
-			</div>
-		  </section>';
-		?>
+		  </section>
+		  <script src="https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js?site=659eab658144ac62b3d366af" type="text/javascript" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+		  <script src="js/webflow.js" type="text/javascript"></script>?>'
+		  ?>
 	</footer>
 </body>
 </html>
