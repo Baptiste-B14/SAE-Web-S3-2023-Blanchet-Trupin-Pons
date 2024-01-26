dfb241abae417764046f7f4a7caff03c38bcddfa
nareio <much.lio@laposte.net>
2024-01-26 03:50:23 +0100

    MAJ demande amis

--- Javaski/mod_profil/modele_profil.php
+++ Javaski/mod_profil/modele_profil.php
@@ -35,6 +35,23 @@
             return $rep;
         }   
 
+        public function envoiDemande(){
+            // recherche id via nom 
+            $req = "
+            SELECT idUtilisateur FROM utilisateur WHERE identifiant=:nomUti";
+            $pdo_req = self::$bdd->prepare($req);
+            $pdo_req->execute([ 'nomUti'=>htmlspecialchars($_POST["nomUti"])]);
+            $rep= $prepare->fetchAll();
+            if (!empty($rep)){
+                // envoie demande
+                $id2 = $rep[0]["idUtilisateur"]
+                $req2 = "
+                INSERT INTO demande_ami (idUtilisateur1, idUtilisateur2) values (:id1, :id2)";
+                $pdo_req2 = self::$bdd->prepare($req);
+                $pdo_req2->execute([ 'id1'=>htmlspecialchars($_SESSION['id']), 'id2'=>htmlspecialchars($id2)]);
+            }
+        }
+
         public function accepterDemandeAmi($idAmi) {
             $req = "
             INSERT INTO est_ami_avec (idUtilisateur1, idUtilisateur2) values (:idAmi, :id);
